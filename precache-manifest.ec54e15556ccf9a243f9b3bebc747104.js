ff02d4ac97388664bee732a7ec89909021edc57c
Shivam Shukla <shivam.dev1097@gmail.com>
2022-07-16 20:42:03 +0530

    Updates

---
+++ precache-manifest.ec54e15556ccf9a243f9b3bebc747104.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99509ffbc5de9382863fa81404fdaddc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2c43d408654ee281d9ba",
+    "url": "/portfolio/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "694738d483e86891c1eb",
+    "url": "/portfolio/static/css/main.74511eb4.chunk.css"
+  },
+  {
+    "revision": "0ae6e65921467bc7fecf",
+    "url": "/portfolio/static/js/2.2e8d7d58.chunk.js"
+  },
+  {
+    "revision": "2c43d408654ee281d9ba",
+    "url": "/portfolio/static/js/3.d03153bc.chunk.js"
+  },
+  {
+    "revision": "694738d483e86891c1eb",
+    "url": "/portfolio/static/js/main.eda9b511.chunk.js"
+  },
+  {
+    "revision": "8c466c9d104fd42446d1",
+    "url": "/portfolio/static/js/runtime-main.2eb64b40.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "7d75af5fe9a98c59f3a1b28bdba77a3e",
+    "url": "/portfolio/static/media/deeplearning.7d75af5f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "985ab49c402b290fff56a08495be58a4",
+    "url": "/portfolio/static/media/stanford.985ab49c.png"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/portfolio/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "cde5f348aa641b8d039e77eee40f4c9d",
+    "url": "/portfolio/static/media/tcs_logo.cde5f348.png"
+  }
+]);
